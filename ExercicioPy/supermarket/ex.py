7dfd10d840ecc101db341b89c2820a2ace6c3441
Alberto-MeloL <albertomeloteco@gmail.com>
2024-05-22 14:57:22 -0300

    Commit autom├ítico antes de desligar o computador

--- ExercicioPy/supermarket/ex.py
+++ ExercicioPy/supermarket/ex.py
@@ -25,3 +25,24 @@ def faturamento_mensal(loja_id, mes):
                 return None
             faturamento_mensal(8,5)
 
+def extrair_faturamento_loja_ano(loja_id):
+    for loja in df_supermarket['lojas']:
+        if loja['id'] == loja_id:
+            for dados_mensais in loja['dados_mensais']:
+                if dados_mensais['mes'] == loja_id:
+                    for faturamento in dados_mensais['faturamento']:
+                        faturamento = sum(dados_mensais['faturamento'])
+                    return faturamento
+                return None
+            faturamento_mensal(8,5)
+
+def faturamento_mensal(loja_id, mes):
+    for loja in df_supermarket['lojas']:
+        if loja['id'] == loja_id:
+            for dados_mensais in loja['dados_mensais']:
+                if dados_mensais['mes'] == mes:
+                    for faturamento in dados_mensais['faturamento']:
+                        faturamento = sum(dados_mensais['faturamento'])
+                    return faturamento
+                return None
+            faturamento_mensal(8,5)
