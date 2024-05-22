49a8ac0684738246343308f0b3101d142099bc8f
Alberto-MeloL <albertomeloteco@gmail.com>
2024-05-22 14:44:40 -0300

    Commit autom├ítico antes de desligar o computador

---
+++ ExercicioPy/supermarket/ex.py
@@ -0,0 +1,27 @@
+
+import pandas as pd
+df_supermarket = pd.read_json('supermarket.json')
+
+def extrair_dado_loja_mes(loja_id, mes):
+    for loja in df_supermarket['lojas']:
+        if loja['id'] == loja_id:
+            for dados_mensais in loja['dados_mensais']:
+                if dados_mensais['mes'] == mes:
+                    return dados_mensais
+    return None
+
+extrair_dado_loja_mes(5,8)
+
+# função para retornar o faturamento do mes
+
+def faturamento_mensal(loja_id, mes):
+    for loja in df_supermarket['lojas']:
+        if loja['id'] == loja_id:
+            for dados_mensais in loja['dados_mensais']:
+                if dados_mensais['mes'] == mes:
+                    for faturamento in dados_mensais['faturamento']:
+                        faturamento = sum(dados_mensais['faturamento'])
+                    return faturamento
+                return None
+            faturamento_mensal(8,5)
+
